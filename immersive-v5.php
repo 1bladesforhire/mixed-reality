482aeb9f82e39daa6378cebf585715a61407b826
Rachel <v-rablad@microsoft.com>
2017-12-04 15:08:07 -0800

    container for v5

--- immersive-v5.php
+++ immersive-v5.php
@@ -2,12 +2,15 @@
 
     include('head.php');
     include('modules/immersive_hero-v5.php');?>
-    <main id="main-content" role="main" data-grid="container">
-        <section data-grid="col-12">
+    <main id="main-content" role="main" >
+       <section data-grid="container">
     <?php 
         include('modules/pivot_tabs-v5.php');
         include('modules/multi-feature-thumb-slide.php');
-        include('modules/feature_set2-v5.php');
+        include('modules/feature_set2-v5.php');?>
+        </section>
+        <section data-grid="col-12">
+    <?php
         include('modules/three_up_dark.php');
         include('modules/two_sections.php');?>
         </section>
