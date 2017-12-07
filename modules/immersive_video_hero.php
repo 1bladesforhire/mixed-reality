f047de61b117a7fd5ec6869fa1b89217a99feb04
Rachel <v-rablad@microsoft.com>
2017-12-07 11:45:22 -0800

    styling header buy box

--- modules/immersive_video_hero.php
+++ modules/immersive_video_hero.php
@@ -1,6 +1,6 @@
 <section class="m-hero-item f-x-center f-y-bottom context-device theme-light" itemscope itemtype="http://schema.org/Product">
     <div class="m-ambient-video">
-        <video role="img" alt="Lean ambient video alt text" poster="assets/WMR_sizzle.png" muted autoplay>
+        <video role="img" alt="Lean ambient video alt text" poster="assets/WMR_sizzle.png" muted loop autoplay>
             <source src="assets/video/acer-1366.mp4" type="video/mp4">
         </video>
     </div>
@@ -8,20 +8,20 @@
     <div class="row details">
         <div class="outer">
 
-            <div data-grid="col-4">
+            <div data-grid="col-3">
                 <picture class="c-image">
-                    <img class="lazyloaded" alt="Acer Windows Mixed Reality Headset with Motion Controllers" src="http://via.placeholder.com/250x250">
+                    <img class="lazyloaded" alt="Acer Windows Mixed Reality Headset" src="assets/video/acer-250X250.jpg">
                 </picture>
             </div>
 
-            <div data-grid="col-4">
+            <div data-grid="col-6">
                 <div class="inner">
-                    <h1 id="page-title" class="c-heading-2 m-v-xxxs p-t-n" itemprop="name">Acer Windows Mixed Reality Headset with Motion Controllers</h1>
-
+                    <h1 id="page-title" class="c-heading-2 m-v-xxxs p-t-n" itemprop="name">Acer Windows Mixed Reality Headset</h1>
+                    <p class="by-ms">By <a href="" class="x-hidden-focus">Microsoft Corporation</a></p>
                     <div class="c-rating f-community-rated f-aggregate" itemscope="" itemtype="http://schema.org/AggregateRating" itemprop="aggregateRating" aria-label="Ratings">
-                        <a href="#ratings-reviews" data-m="{&quot;cN&quot;:&quot;RatingsReviewsSummaryLink&quot;,&quot;id&quot;:&quot;nn49c1c2c1m1r1a2&quot;,&quot;sN&quot;:49,&quot;aN&quot;:&quot;c1c2c1m1r1a2&quot;}">
+                        <a href="#ratings-reviews" data-m="{"cN":"RatingsReviewsSummaryLink","id":"nn49c1c2c1m1r1a2","sN":49,"aN":"c1c2c1m1r1a2"}">
                             <meta class="srv_microdata" itemprop="ratingValue" content="3.90000009536743">
-                            <meta class="srv_microdata" itemprop="ratingCount" content="2043">
+                            <meta class="srv_microdata" itemprop="ratingCount" content="40">
                             <p class="x-screen-reader">Rated: 3.9 stars out of 5</p>
                             <div aria-hidden="true">
                                 <span class="c-glyph f-full"></span>
@@ -30,18 +30,19 @@
                                 <span class="c-glyph f-half"></span>
                                 <span class="c-glyph f-none"></span>
                             </div>
-                            <span class="c-meta-text"> 3,543 </span>
+                            <span class="c-meta-text"> 40 </span>
                             <span class="x-screen-reader"> reviews </span>
                         </a>
                     </div>
-
-                    <div class="cli_affirmation-message">
-                        <div class="cli_affirmation-message-text ">Choose your subscription:</div>
-                    </div>
-
+                    <ul class="bullets">
+                        <li>Flip Visor means that you don’t have to remove the headset, just lift the visor up and engage with the real world again.</li>
+                        <li>Inside-Out Tracking automatically changes the perspective of the outside world without needing to install additional sensors in the room.</li>
+                        <li>Spatial audio lets you hear sounds from specific locations just like in the real world​</li>
+                        <li>Windows Mixed Reality Headset Controllers included</li>
+                    </ul>
                 </div>
             </div>
-            <div data-grid="col-4">
+            <div data-grid="col-3">
                 <div class="inner">
 
                 <div class="context-configuration-buttons">
