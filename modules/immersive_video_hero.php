33095f53b9730b4934505c0f15f812f02946c631
Rachel <v-rablad@microsoft.com>
2017-12-07 13:24:25 -0800

    styling

--- modules/immersive_video_hero.php
+++ modules/immersive_video_hero.php
@@ -45,12 +45,31 @@
             </div>
             <div data-grid="col-3">
                 <div class="inner">
+                <div class="c-price">
+                    <div class="price-text srv_price">
+                        <span aria-label="Price" class="x-hidden-focus">$399.99</span>
+                    </div>
 
-                <div class="context-configuration-buttons">
-                    <a class="c-call-to-action c-glyph">Add to Cart</a>
-                    <a class="c-call-to-action c-glyph secondary">Find in Store</a>
+                    <div class="srv_microdata" itemprop="offers" itemscope="" itemtype="http://schema.org/Offer">
+                        <meta itemprop="price" content="399">
+                        <meta itemprop="priceCurrency" content="USD">
+                    </div>
                 </div>
-
+                <p class="c-caption-3 x-hidden-focus">
+                    Free shipping. Free returns.
+                </p>
+                <div class="context-configuration-buttons">
+                    <a class="add-cart">Add to Cart</a>
+                    <a class="store">Find in Store</a
+                </div>
+                <div class="hr-or">
+                    <span class="x-hidden-focus">Or</span>
+                    <hr>
+                </div>
+                <div class="leading-action">
+                    <p class="save">Save $400 instantly</p>  
+                    <p class="bundle"> Bundle this headset with an Acer Aspire Nitro 5 for instant savings. <a href="#" class="f-blue">Bundle now</a></p>
+                </div>
                     
                 </div>
             </div>
