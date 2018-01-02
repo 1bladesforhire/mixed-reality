6a672421ad3fe24bfc092d7bd71aaa8e61abfff2
Rachel <v-rablad@microsoft.com>
2018-01-02 09:51:17 -0800

    single image header

--- modules/immersive-hero-img.php
+++ modules/immersive-hero-img.php
@@ -1,109 +1,15 @@
 <div class="hero fixed-car v2">
-<div class="m-immersive-hero">
-    <div class="c-carousel f-multi-slide" role="region" aria-label="Label">
-        <div class="c-sequence-indicator" role="tablist">
-            <button role="tab" aria-selected="true" aria-label="View default align top" aria-controls="default-align-top" title="default-align-top"></button>
-            <button role="tab" aria-selected="false" aria-label="View default align bottom" aria-controls="default-align-bottom" title="default-align-bottom"></button>
-            <button role="tab" aria-selected="false" aria-label="View no image align top" aria-controls="no-image-align-top" title="no-image-align-top"></button>
-            <button role="tab" aria-selected="false" aria-label="View no image align bottom" aria-controls="no-image-align-bottom" title="no-image-align-bottom"></button>
-        </div>
-        <button class="c-flipper f-previous f-large" aria-hidden="true" tabindex="-1"></button>
-        <button class="c-flipper f-next f-large" aria-hidden="true" tabindex="-1"></button>
-        <div itemscope itemtype="http://schema.org/ItemList">
-            <ul>
-                <li id="default-align-top" data-f-theme="light" class="f-active">
-                    <section class="m-immersive-hero-item theme-light f-align-top">
-                        <picture class="c-image">
-                            <source srcset="assets/video/acer-hero.jpg" media="(min-width: 1779px)">
-                            <source srcset="assets/video/acer-hero.jpg" media="(min-width:1400px)">
-                            <source srcset="assets/video/acer-hero.jpg" media="(min-width:1084px)">
-                            <source srcset="assets/video/acer-hero.jpg" media="(min-width:768px)">
-                            <source srcset="assets/video/acer-hero.jpg" media="(min-width:540px)">
-                            <source srcset="assets/video/acer-hero.jpg" media="(min-width:0)">
-                            <img srcset="assets/video/acer-hero.jpg" src="assets/video/acer-hero.jpg" alt="Placeholder with grey background and dimension watermark without any imagery">
-                        </picture>
-
-                    </section>
-                </li>
-                <li id="default-align-bottom" data-f-theme="light">
-                    <section class="m-immersive-hero-item theme-light f-align-bottom">
-                        <picture class="c-image">
-                            <source srcset="http://placehold.it/2400x1600" media="(min-width: 1779px)">
-                            <source srcset="http://placehold.it/1778x1185" media="(min-width:1400px)">
-                            <source srcset="http://placehold.it/1399x931" media="(min-width:1084px)">
-                            <source srcset="http://placehold.it/1083x1805" media="(min-width:768px)">
-                            <source srcset="http://placehold.it/768x1280" media="(min-width:540px)">
-                            <source srcset="http://placehold.it/539x898" media="(min-width:0)">
-                            <img srcset="http://placehold.it/1399x931" src="http://placehold.it/1399x931" alt="Placeholder with grey background and dimension watermark without any imagery">
-                        </picture>
-                        <div>
-                            <picture class="c-image">
-                                <source srcset="http://placehold.it/2048x1152/666666/171717" media="(min-width: 1779px)">
-                                <source srcset="http://placehold.it/1778x1000/666666/171717" media="(min-width:1400px)">
-                                <source srcset="http://placehold.it/1399x787/666666/171717" media="(min-width:1084px)">
-                                <source srcset="http://placehold.it/1083x812/666666/171717" media="(min-width:768px)">
-                                <source srcset="http://placehold.it/767x575/666666/171717" media="(min-width:540px)">
-                                <source srcset="http://placehold.it/539x404/666666/171717" media="(min-width:0)">
-                                <img srcset="http://placehold.it/1399x787/666666/171717" src="http://placehold.it/1399x787/666666/171717" alt="Placeholder image">
-                            </picture>
-                            <div>
-                                <h1 class="c-heading">Default align bottom</h1>
-                                <p class="c-subheading">Subheading</p>
-                                <a href="#" class="c-call-to-action c-glyph" aria-label="More verbose call to action text">
-                                    <span>CALL TO ACTION</span>
-                                </a>
-                            </div>
-                        </div>
-                    </section>
-                </li>
-                <li id="no-image-align-top" data-f-theme="light">
-                    <section class="m-immersive-hero-item theme-light f-align-top">
-                        <picture class="c-image">
-                            <source srcset="http://placehold.it/2400x1600/666666/171717" media="(min-width: 1779px)">
-                            <source srcset="http://placehold.it/1778x1185/666666/171717" media="(min-width:1400px)">
-                            <source srcset="http://placehold.it/1399x931/666666/171717" media="(min-width:1084px)">
-                            <source srcset="http://placehold.it/1083x1805/666666/171717" media="(min-width:768px)">
-                            <source srcset="http://placehold.it/768x1280/666666/171717" media="(min-width:540px)">
-                            <source srcset="http://placehold.it/539x898/666666/171717" media="(min-width:0)">
-                            <img srcset="http://placehold.it/1399x931/666666/171717" src="http://placehold.it/1399x931/666666/171717" alt="Placeholder with grey background and dimension watermark without any imagery">
-                        </picture>
-                        <div>
-                            <div>
-                                <h1 class="c-heading">No image align top</h1>
-                                <p class="c-subheading">Subheading</p>
-                                <a href="#" class="c-call-to-action c-glyph" aria-label="More verbose call to action text">
-                                    <span>CALL TO ACTION</span>
-                                </a>
-                            </div>
-                        </div>
-                    </section>
-                </li>
-                <li id="no-image-align-bottom" data-f-theme="light">
-                    <section class="m-immersive-hero-item theme-light f-align-bottom">
-                        <picture class="c-image">
-                            <source srcset="http://placehold.it/2400x1600" media="(min-width: 1779px)">
-                            <source srcset="http://placehold.it/1778x1185" media="(min-width:1400px)">
-                            <source srcset="http://placehold.it/1399x931" media="(min-width:1084px)">
-                            <source srcset="http://placehold.it/1083x1805" media="(min-width:768px)">
-                            <source srcset="http://placehold.it/768x1280" media="(min-width:540px)">
-                            <source srcset="http://placehold.it/539x898" media="(min-width:0)">
-                            <img srcset="http://placehold.it/1399x931" src="http://placehold.it/1399x931" alt="Placeholder with grey background and dimension watermark without any imagery">
-                        </picture>
-                        <div>
-                            <div>
-                                <h1 class="c-heading">No image align bottom</h1>
-                                <p class="c-subheading">Subheading</p>
-                                <a href="#" class="c-call-to-action c-glyph" aria-label="More verbose call to action text">
-                                    <span>CALL TO ACTION</span>
-                                </a>
-                            </div>
-                        </div>
-                    </section>
-                </li>
-            </ul>
-        </div>
-    </div>
-</div>
+<section class="m-immersive-hero f-transparent theme-light" itemscope itemtype="http://schema.org/SoftwareApplication">
+    <picture>
+        <source srcset="assets/video/acer-hero.jpg" media="(min-width: 1779px)">
+        <source srcset="assets/video/acer-hero.jpg" media="(min-width: 1400px)">
+        <source srcset="assets/video/acer-hero.jpg" media="(min-width: 1084px)">
+        <source srcset="assets/video/acer-hero.jpg" media="(min-width:768px)">
+        <source srcset="assets/video/acer-hero.jpg" media="(min-width:540px)">
+        <source srcset="assets/video/acer-hero.jpg" media="(min-width:0)">
+        <img srcset="assets/video/acer-hero.jpg" src="assets/video/acer-hero.jpg" alt="Placeholder with grey background and dimension watermark without any imagery">
+    </picture>
+</section>
     <div data-grid="container">
 
         <div class="buybox v2">
