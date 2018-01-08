2fddf459840a3bbd0bd98e3e4946d327850a59bd
Rachel <v-rablad@microsoft.com>
2018-01-08 14:34:17 -0800

    version 2 carousel

---
+++ immersive-car-2.php
@@ -0,0 +1,20 @@
+<?php
+
+    include('head-waffle.php');
+    include('modules/im-hero-car-2.php');
+    ?>
+    <main id="main-content main" class="optionb" role="main" >
+
+    <?php 
+        include('modules/pivot_tabs-video.php');
+        // include('modules/multi-feature-thumb-slide.php');
+        // include('modules/feature_set2-v5.php');
+        // include('modules/three_up_dark.php');
+        // include('modules/two_sections.php');
+        ?>
+
+</main>
+    <?php
+    include('footer-v2.php');
+?>
+?>
---
+++ modules/im-hero-car-2.php
@@ -0,0 +1,178 @@
+<div class="hero fixed-car v2">
+<div class="m-immersive-hero">
+    <div class="c-carousel f-multi-slide" role="region" aria-label="Label">
+        <div class="c-sequence-indicator" role="tablist">
+            <button role="tab" aria-selected="true" aria-label="View default align top" aria-controls="default-align-top" title="default-align-top"></button>
+            <button role="tab" aria-selected="false" aria-label="View default align bottom" aria-controls="default-align-bottom" title="default-align-bottom"></button>
+            <button role="tab" aria-selected="false" aria-label="View no image align top" aria-controls="no-image-align-top" title="no-image-align-top"></button>
+            <!-- <button role="tab" aria-selected="false" aria-label="View no image align bottom" aria-controls="no-image-align-bottom" title="no-image-align-bottom"></button>
+            <button role="tab" aria-selected="false" aria-label="View default align bottom" aria-controls="default-align-bottom" title="default-align-bottom"></button>
+            <button role="tab" aria-selected="false" aria-label="View no image align top" aria-controls="no-image-align-top" title="no-image-align-top"></button> -->
+        </div>
+        <button class="c-flipper f-previous f-large" aria-hidden="true" tabindex="-1"></button>
+        <button class="c-flipper f-next f-large" aria-hidden="true" tabindex="-1"></button>
+        <div itemscope itemtype="http://schema.org/ItemList">
+            <ul>
+                <!-- <li id="default-align-top" data-f-theme="light" class="f-active">
+                    <section class="m-immersive-hero-item theme-light f-align-top">
+                        <picture class="c-image">
+                            <source srcset="assets/video/acer-hero.jpg" media="(min-width: 1779px)">
+                            <source srcset="assets/video/acer-hero.jpg" media="(min-width:1400px)">
+                            <source srcset="assets/video/acer-hero.jpg" media="(min-width:1084px)">
+                            <source srcset="assets/video/acer-hero.jpg" media="(min-width:768px)">
+                            <source srcset="assets/video/acer-hero.jpg" media="(min-width:540px)">
+                            <source srcset="assets/video/acer-hero.jpg" media="(min-width:0)">
+                            <img srcset="assets/video/acer-hero.jpg" src="assets/video/acer-hero.jpg" alt="Placeholder with grey background and dimension watermark without any imagery">
+                        </picture>
+
+                    </section>
+                </li> -->
+                <li id="default-align-top" data-f-theme="light" class="f-active">> 
+                    <section class="m-immersive-hero-item theme-light f-align-top">
+
+                        <picture class="c-image">
+                            <source srcset="assets/video/acer-hero-macro-1.jpg" media="(min-width: 1779px)">
+                            <source srcset="assets/video/acer-hero-macro-1.jpg" media="(min-width:1400px)">
+                            <source srcset="assets/video/acer-hero-macro-1.jpg" media="(min-width:1084px)">
+                            <source srcset="assets/video/acer-hero-macro-1.jpg" media="(min-width:768px)">
+                            <source srcset="assets/video/acer-hero-macro-1.jpg" media="(min-width:540px)">
+                            <source srcset="assets/video/acer-hero-macro-1.jpg" media="(min-width:0)">
+                            <img srcset="assets/video/acer-hero-macro-1.jpg" src="assets/video/acer-hero-macro-1.jpg" alt="Placeholder with grey background and dimension watermark without any imagery">
+                        </picture>
+
+                    </section>
+                </li>
+                <li id="default-align-top" data-f-theme="light" >
+                    <section class="m-immersive-hero-item theme-light f-align-top">
+                        <picture class="c-image">
+                            <source srcset="assets/video/acer-hero-macro-2.jpg" media="(min-width: 1779px)">
+                            <source srcset="assets/video/acer-hero-macro-2.jpg" media="(min-width:1400px)">
+                            <source srcset="assets/video/acer-hero-macro-2.jpg" media="(min-width:1084px)">
+                            <source srcset="assets/video/acer-hero-macro-2.jpg" media="(min-width:768px)">
+                            <source srcset="assets/video/acer-hero-macro-2.jpg" media="(min-width:540px)">
+                            <source srcset="assets/video/acer-hero-macro-2.jpg" media="(min-width:0)">
+                            <img srcset="assets/video/acer-hero-macro-2.jpg" src="assets/video/acer-hero-macro-2.jpg" alt="Placeholder with grey background and dimension watermark without any imagery">
+                        </picture>
+
+                    </section>
+                </li>
+                <li id="default-align-top" data-f-theme="light" >
+                <section class="m-immersive-hero-item theme-light f-align-top">
+                    <picture class="c-image">
+                        <source srcset="assets/video/acer-hero-macro-3.jpg" media="(min-width: 1779px)">
+                        <source srcset="assets/video/acer-hero-macro-3.jpg" media="(min-width:1400px)">
+                        <source srcset="assets/video/acer-hero-macro-3.jpg" media="(min-width:1084px)">
+                        <source srcset="assets/video/acer-hero-macro-3.jpg" media="(min-width:768px)">
+                        <source srcset="assets/video/acer-hero-macro-3.jpg" media="(min-width:540px)">
+                        <source srcset="assets/video/acer-hero-macro-3.jpg" media="(min-width:0)">
+                        <img srcset="assets/video/acer-hero-macro-3.jpg" src="assets/video/acer-hero-macro-3.jpg" alt="Placeholder with grey background and dimension watermark without any imagery">
+                    </picture>
+
+                </section>
+            </li>
+            <!-- <li id="default-align-top" data-f-theme="light" >
+                    <section class="m-immersive-hero-item theme-light f-align-top">
+                        <picture class="c-image">
+                            <source srcset="assets/video/acer-hero-5.jpg" media="(min-width: 1779px)">
+                            <source srcset="assets/video/acer-hero-5.jpg" media="(min-width:1400px)">
+                            <source srcset="assets/video/acer-hero-5.jpg" media="(min-width:1084px)">
+                            <source srcset="assets/video/acer-hero-5.jpg" media="(min-width:768px)">
+                            <source srcset="assets/video/acer-hero-5.jpg" media="(min-width:540px)">
+                            <source srcset="assets/video/acer-hero-5.jpg" media="(min-width:0)">
+                            <img srcset="assets/video/acer-hero-5.jpg" src="assets/video/acer-hero-5.jpg" alt="Placeholder with grey background and dimension watermark without any imagery">
+                        </picture>
+
+                    </section>
+                </li>
+                <li id="default-align-top" data-f-theme="light" >
+                    <section class="m-immersive-hero-item theme-light f-align-top">
+                        <picture class="c-image">
+                            <source srcset="assets/video/acer-hero-6.jpg" media="(min-width: 1779px)">
+                            <source srcset="assets/video/acer-hero-6.jpg" media="(min-width:1400px)">
+                            <source srcset="assets/video/acer-hero-6.jpg" media="(min-width:1084px)">
+                            <source srcset="assets/video/acer-hero-6.jpg" media="(min-width:768px)">
+                            <source srcset="assets/video/acer-hero-6.jpg" media="(min-width:540px)">
+                            <source srcset="assets/video/acer-hero-6.jpg" media="(min-width:0)">
+                            <img srcset="assets/video/acer-hero-6.jpg" src="assets/video/acer-hero-6.jpg" alt="Placeholder with grey background and dimension watermark without any imagery">
+                        </picture>
+
+                    </section>
+                </li> -->
+            </ul>
+        </div>
+    </div>
+</div>
+    <div data-grid="container">
+
+        <div class="buybox v2">
+            <div data-grid="col-3">
+                <picture class="c-image">
+                    <img class="lazyloaded" alt="Acer Windows Mixed Reality Headset" src="assets/video/acer-250X250.jpg">
+                </picture>
+            </div>
+            <div data-grid="col-6">
+                <div class="inner">
+                    <h1 id="page-title" class="c-heading-2 m-v-xxxs p-t-n" itemprop="name">Acer Windows Mixed Reality Headset</h1>
+                    <p class="by-ms">By <a href="" class="x-hidden-focus f-blue">Microsoft Corporation</a></p>
+
+                    <div class="c-rating f-community-rated f-aggregate" itemscope="" itemtype="http://schema.org/AggregateRating" itemprop="aggregateRating" aria-label="Ratings">
+                        <a href="#ratings-reviews" data-m="{" cN ":"RatingsReviewsSummaryLink ","id ":"nn49c1c2c1m1r1a2 ","sN ":49,"aN ":"c1c2c1m1r1a2 "}">
+                            <meta class="srv_microdata" itemprop="ratingValue" content="3.90000009536743">
+                            <meta class="srv_microdata" itemprop="ratingCount" content="40">
+                            <p class="x-screen-reader">Rated: 3.9 stars out of 5</p>
+                            <div aria-hidden="true">
+                                <span class="c-glyph f-full"></span>
+                                <span class="c-glyph f-full"></span>
+                                <span class="c-glyph f-full"></span>
+                                <span class="c-glyph f-half"></span>
+                                <span class="c-glyph f-none"></span>
+                            </div>
+                            <span class="c-meta-text"> 40</span>
+                            <a href="#" class="f-blue">Write a review</a>
+                            <span class="x-screen-reader"> reviews </span>
+                        </a>
+                    </div>
+                    <p class="social"><a href="#" class="c-glyph f-heart">Favorite</a> <a href="#" class="c-glyph f-share"> Share</a></p>
+                    <ul class="bullets">
+                        <li>Flip Visor means that you don’t have to remove the headset, just lift the visor up and engage with the real world again.</li>
+                        <li>Inside-Out Tracking automatically changes the perspective of the outside world without needing to install additional sensors in the room.</li>
+                        <li>Spatial audio lets you hear sounds from specific locations just like in the real world​</li>
+                        <li>Windows Mixed Reality Headset Controllers included</li>
+                    </ul>
+                </div>
+            </div>
+            <div data-grid="col-3">
+                <div class="inner">
+                    <div class="c-price">
+                        <div class="price-text srv_price">
+                            <span aria-label="Price" class="x-hidden-focus">$399.99</span>
+                        </div>
+
+                        <div class="srv_microdata" itemprop="offers" itemscope="" itemtype="http://schema.org/Offer">
+                            <meta itemprop="price" content="399">
+                            <meta itemprop="priceCurrency" content="USD">
+                        </div>
+                    </div>
+                    <p class="c-caption-3 x-hidden-focus">
+                        Free shipping. Free returns.
+                    </p>
+                    <div class="context-configuration-buttons">
+                        <a class="add-cart">Add to cart</a>
+                        <div class="hr-or">
+                            <span class="x-hidden-focus">Or</span>
+                            <hr>
+                        </div>
+                        <a class="store">Find in store</a
+                    </div>
+                
+                <div class="leading-action">
+                    <p class="save">Save $400 instantly</p>  
+                    <p class="bundle"> Bundle this headset with an Acer Aspire Nitro 5 for instant savings. <a href="#" class="f-blue">Bundle now</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
